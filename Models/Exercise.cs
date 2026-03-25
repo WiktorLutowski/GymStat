819ee388c00b2f82e030f341a514e7a2791f5b1e
Wiktor Lutowski <wiktor.luto@gmail.com>
2026-03-25 19:08:21 +0100

    Add models

---
+++ Models/Exercise.cs
@@ -0,0 +1,8 @@
+﻿namespace GymStat.Models
+{
+    public class Exercise(string exerciseImageFileName, string exerciseName)
+    {
+        public string ExerciseName { get; set; } = exerciseName;
+        public string ExerciseImageFileName { get; set; } = exerciseImageFileName;
+    }
+}
---
+++ Models/ExerciseResult.cs
@@ -0,0 +1,10 @@
+﻿namespace GymStat.Models
+{
+    public class ExerciseResult(Exercise exercise, int repetitions, float weight, DateOnly date)
+    {
+        public Exercise Exercise { get; set; } = exercise;
+        public float Weight { get; set; } = weight;
+        public int Repetitions { get; set; } = repetitions;
+        public DateOnly Date { get; set; } = date;
+    }
+}
