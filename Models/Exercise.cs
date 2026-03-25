7eae675a1622a1e9d3025212436a3155a5048d85
Wiktor Lutowski <wiktor.luto@gmail.com>
2026-03-25 20:22:01 +0100

    Refactor constructor params

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -1,6 +1,6 @@
 ﻿namespace GymStat.Models
 {
-    public class Exercise(string exerciseImageFileName, string exerciseName)
+    public class Exercise(string exerciseName, string exerciseImageFileName)
     {
         public string ExerciseName { get; set; } = exerciseName;
         public string ExerciseImageFileName { get; set; } = exerciseImageFileName;
