353445133a7d02df3fd2bd920c7e7f06be9137ac
Wiktor Lutowski <wiktor.luto@gmail.com>
2026-03-25 20:26:43 +0100

    Add set model

--- Models/ExerciseResult.cs
+++ Models/ExerciseResult.cs
@@ -1,10 +1,9 @@
 ﻿namespace GymStat.Models
 {
-    public class ExerciseResult(Exercise exercise, int repetitions, float weight, DateOnly date)
+    public class ExerciseResult(Exercise exercise, DateOnly date)
     {
         public Exercise Exercise { get; set; } = exercise;
-        public float Weight { get; set; } = weight;
-        public int Repetitions { get; set; } = repetitions;
+        public List<Set> Sets { get; set; } = [];
         public DateOnly Date { get; set; } = date;
     }
 }
---
+++ Models/Set.cs
@@ -0,0 +1,8 @@
+﻿namespace GymStat.Models
+{
+    public class Set(int repetitions, float weight)
+    {
+        public float Weight { get; set; } = weight;
+        public int Repetitions { get; set; } = repetitions;
+    }
+}
