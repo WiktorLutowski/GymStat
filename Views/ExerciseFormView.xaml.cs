736c31e37392041d72d29e0bc764b5a468791b1b
Wiktor Lutowski <wiktor.luto@gmail.com>
2026-03-26 21:52:42 +0100

    add exercise form validation

--- Views/ExerciseFormView.xaml.cs
+++ Views/ExerciseFormView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,24 @@ namespace GymStat.Views
         public ExerciseFormView()
         {
             InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string text = (string)e.DataObject.GetData(typeof(string));
+
+                if (new Regex("[^0-9]+").IsMatch(text))
+                    e.CancelCommand();
+            }
+            else
+                e.CancelCommand();
         }
     }
 }
